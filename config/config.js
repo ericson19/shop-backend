f6cf70dd02ad194b55f9ae053ff2da2cba3e5068
Eric <ericanox@gmail.com>
2026-05-06 04:36:25 +0100

    port updated

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: 3306,
+    port: process.env.DB_PORT,
     dialectOptions: {
       ssl: {
         require: true,
@@ -21,13 +21,14 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: 3306,
+    port: process.env.DB_PORT,
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
